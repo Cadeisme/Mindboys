9012c19b7443e10890e152bf340cd6a01aff092c
lougehrig10 <lougehrig10@gmail.com>
2017-10-20 12:43:40 -0500

    Natives, Windowing and Game loop

---
+++ Mindboys/src/io/github/fairyfruit/mindboys/main/Keymanager.java
@@ -0,0 +1,12 @@
+package io.github.fairyfruit.mindboys.main;
+
+public class Keymanager {
+
+	public static final int UP = 1;
+	public static final int DOWN = 2;
+	public static final int LEFT = 3;
+	public static final int RIGHT = 4;
+	
+	public static boolean[] keys = new boolean[5];
+	
+}
